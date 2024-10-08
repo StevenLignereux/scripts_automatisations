47f2813ccc7628e72d574d176df274c61f79bad9
Steven Lignereux <slignere@gmail.com>
2024-10-08 08:07:38 +0200

    📦 NEW: Lire des fichiers

---
+++ fichiers_texte_2.py
@@ -0,0 +1,18 @@
+# FICHIERS
+#
+# ouvrir (open) <- nom du fichier
+# ecrire (write) / lire (read)
+#fermer (close)
+#
+
+f = open("mon_fichier.txt", "r")
+
+"""texte = f.readline()
+print(texte, end="")
+texte = f.readline()
+print(texte)"""
+
+for line in f:
+    print(line, end="")
+
+f.close()
